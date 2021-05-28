628c3cc7c28b912f550178f03bc69906961039b0
Bryan Gonzalez <bryangonzalezm@outlook.com>
2021-05-28 11:19:39 -0500

    Add project files.

---
+++ Mexty/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Mexty.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mexty.MVVM.ViewModel {
+    class MainViewModel : ObservableObject {
+
+        public AdminViewModel AdmnVm { get; set; }
+
+        private object _currenView;
+
+        public object CurrentView {
+            get { return _currenView; }
+            set { _currenView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel() {
+            AdmnVm = new AdminViewModel();
+            CurrentView = AdmnVm;
+
+        }
+    }
+}
