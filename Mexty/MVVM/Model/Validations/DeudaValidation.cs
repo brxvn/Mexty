6b8a3ae628876f25492086b8fc538040ad6b0d4b
fwxzxh <jdemeca01@gmail.com>
2021-06-28 15:09:40 -0500

    Deuda validator added

---
+++ Mexty/MVVM/Model/Validations/DeudaValidation.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Mexty.MVVM.Model.DataTypes;
+
+namespace Mexty.MVVM.Model.Validations {
+    /// <summary>
+    /// Clase para la validación de datos de un objeto Deuda.
+    /// </summary>
+    public class DeudaValidation : AbstractValidator<Deuda> {
+        public DeudaValidation() {
+            RuleFor(deuda => deuda.Debe.ToString())
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty().WithMessage("La deuda no puede estar vacia, en caso de no tener deuda poner 0")
+                .Length(1, 5).WithMessage("La deuda dada exede el largo permitido que es de 5 dijitos.")
+                .Must(Validations.BeAValidFloat).WithMessage("No es un número valido, tiene caracteres prohibidos.");
+        }
+    }
+}
