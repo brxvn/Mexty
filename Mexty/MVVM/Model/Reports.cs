7d96d7b39ee78f5af15ab20926fc080a9c93fa74
brxvn <bryangonzalezm@outlook.com>
2021-07-29 14:24:45 -0500

    Reportes Changed

--- Mexty/MVVM/Model/Reports.cs
+++ Mexty/MVVM/Model/Reports.cs
@@ -1,5 +1,6 @@
 ﻿using Common.Logging;
 using iText.IO.Image;
+using iText.Kernel.Geom;
 using iText.Kernel.Pdf;
 using iText.Layout;
 using iText.Layout.Element;
@@ -33,37 +34,37 @@ namespace Mexty.MVVM.Model {
             // TODO
             Directory.CreateDirectory(@"C:\Mexty\Reportes\");
             PdfDocument pdfDocument = new PdfDocument(new PdfWriter(new FileStream($@"C:\Mexty\Reportes\Reporte-{_date}.pdf", FileMode.Create, FileAccess.Write)));
-            Document document = new Document(pdfDocument);
-
+            Document document = new Document(pdfDocument, PageSize.A7);
+            document.SetMargins(5,5,5,5);
             // Load image from disk
             ImageData imageData = ImageDataFactory.Create(@"C:\Mexty\Brand\LogoReportes.png");
             // Create layout image object and provide parameters. Page number = 1
-            Image image = new Image(imageData).ScaleAbsolute(150, 50);
+            Image image = new Image(imageData).ScaleAbsolute(100, 30);
+            image.SetHorizontalAlignment(iText.Layout.Properties.HorizontalAlignment.CENTER);
             // This adds the image to the page
             document.Add(image);
 
             string titulo = "Reporte de inventario de Sucursal";
 
-            document.Add(new Paragraph(titulo).SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER).SetFontSize(15));
+            document.Add(new Paragraph(titulo).SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER).SetFontSize(8));
 
 
-            string[] columnas = { "ID", "Tipo de Producto", "Nombre", "Medida", "Piezas", "Cantidad" };
-            float[] tamaños = { 1, 3, 3, 3, 2, 2 };
+            string[] columnas = { "ID", "Tipo de Producto", "Nombre", "Piezas", "Cantidad" };
+            float[] tamaños = { 1, 3, 3, 2, 2 };
 
             Table table = new Table(UnitValue.CreatePercentArray(tamaños));
             table.SetWidth(UnitValue.CreatePercentValue(100));
 
             foreach (string columa in columnas) {
-                table.AddHeaderCell(new Cell().Add(new Paragraph(columa).SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER)));
+                table.AddHeaderCell(new Cell().Add(new Paragraph(columa).SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER).SetFontSize(8)));
             }
 
             foreach (var item in data) {
-                table.AddCell(new Cell().Add(new Paragraph(item.IdProducto.ToString())));
-                table.AddCell(new Cell().Add(new Paragraph(item.TipoProducto.ToString())));
-                table.AddCell(new Cell().Add(new Paragraph(item.NombreProducto.ToString())));
-                table.AddCell(new Cell().Add(new Paragraph(item.Medida.ToString())));
-                table.AddCell(new Cell().Add(new Paragraph(item.Piezas.ToString())));
-                table.AddCell(new Cell().Add(new Paragraph(item.Cantidad.ToString())));
+                table.AddCell(new Cell().Add(new Paragraph(item.IdProducto.ToString()).SetFontSize(8)));
+                table.AddCell(new Cell().Add(new Paragraph(item.TipoProducto.ToString()).SetFontSize(8)));
+                table.AddCell(new Cell().Add(new Paragraph(item.NombreProducto.ToString()).SetFontSize(8)));
+                table.AddCell(new Cell().Add(new Paragraph(item.Piezas.ToString()).SetFontSize(8)));
+                table.AddCell(new Cell().Add(new Paragraph(item.Cantidad.ToString()).SetFontSize(8)));
             }
 
             document.Add(table);
