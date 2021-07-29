860b93b3310ae714dd1fdf92985fcc41ff7a3bf5
fwxzxh <jdemeca01@gmail.com>
2021-07-29 14:10:20 -0500

    Fixed combobox in ViewInventario

--- Mexty/MVVM/View/ReportesViews/ReportesViewInventario.xaml.cs
+++ Mexty/MVVM/View/ReportesViews/ReportesViewInventario.xaml.cs
@@ -3,19 +3,32 @@ using iText.Layout;
 using iText.Layout.Element;
 using Mexty.MVVM.Model;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Threading;
+using Common.Logging;
 
 namespace Mexty.MVVM.View.ReportesViews {
     /// <summary>
     /// Interaction logic for ReportesViewInventario.xaml
     /// </summary>
     public partial class ReportesViewInventario : UserControl {
+        private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod()?.DeclaringType);
+
         Reports report = new Reports();
         public ReportesViewInventario() {
-            InitializeComponent();
+            try {
+                InitializeComponent();
+                FillData();
+                Log.Debug("Se han llenado los campos de reportes de inventario de manera exitosa.");
+
+            }
+            catch (Exception e) {
+                Log.Error("Ha ocurrido un error al llenar los campos de reportes inventario");
+                Log.Error($"Error {e.Message}");
+            }
 
             
             DispatcherTimer timer = new DispatcherTimer();
@@ -33,5 +46,28 @@ namespace Mexty.MVVM.View.ReportesViews {
             time.Content = DateTime.Now.ToString("G");
         }
 
+        /// <summary>
+        /// Método que se encarga de llenar los campos de Reportes inventario.
+        /// </summary>
+        private void FillData() {
+            try {
+                var dataSucursal = Database.GetTablesFromSucursales();
+                foreach (var sucu in dataSucursal) {
+                    ComboSucursal.Items.Add(sucu.NombreTienda);
+                }
+                Log.Debug("Se ha llenado el combo de sucursal");
+
+                var dataProductos = Database.GetTablesFromProductos();
+                foreach (var pro in dataProductos) {
+                    ComboTipo.Items.Add($"{pro.TipoProducto} {pro.NombreProducto}");
+                }
+                Log.Debug("Se ha llenado el combo de productos.");
+
+            }
+            catch (Exception e) {
+                Log.Error("Ha ocurrido un error al llenar los campos de reportes inventario");
+                Log.Error($"Error {e.Message}");
+            }
+        }
     }
 }
