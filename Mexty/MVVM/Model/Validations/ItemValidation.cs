b4a6794e3da677bc7e515c821eea70b512979aea
Fxzxzh <jdemeca01@gmail.com>
2021-12-08 20:22:09 -0600

    inventario cantidad hotfix

--- Mexty/MVVM/Model/Validations/ItemValidation.cs
+++ Mexty/MVVM/Model/Validations/ItemValidation.cs
@@ -8,15 +8,15 @@ namespace Mexty.MVVM.Model.Validations {
 
             RuleFor(item => item.Cantidad)
                 .Cascade(CascadeMode.Stop)
-                .NotEmpty().WithMessage("La cantidad no puede estar vacia, en todo caso poner 0.")
-                .GreaterThan(0).WithMessage("La cantidad no puede ser menor a 0.")
+                .NotNull().WithMessage("La cantidad no puede estar vacía.")
+                .GreaterThanOrEqualTo(0).WithMessage("La cantidad no puede ser menor a 0.")
                 .LessThan(99999999).WithMessage("La cantidad es demasiado grande para ser valida");
                 //.Must(Validations.BeAValidNumber).WithMessage("No es una cantidad valida");
 
             RuleFor(item => item.Comentario)
                 .Cascade(CascadeMode.Stop)
                 .Length(0, 100).WithMessage("El comentario es demasiado largo tiene {TotalLength} caracteres y debe tener no más de 100.")
-                .Must(Validations.BeAValidText).WithMessage("El comentario tiene caracteres invalidos.");
+                .Must(Validations.BeAValidText).WithMessage("El comentario tiene caracteres inválidos.");
         }
     }
 }
